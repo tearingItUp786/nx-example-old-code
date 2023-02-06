8f7ea9e0f04a34c1dbf3d5dd0ab6e621c6bb52b3
Taranveer (Taran) Bains <taranveer786@gmail.com>
2023-02-06 13:05:31 -0800

    "update webpack to handle tsconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,53 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    entry: './src/main.tsx',
-    devtool: 'inline-source-map',
-    output: {
-        path: path.join(__dirname, '/dist'),
-        filename: 'bundle.js',
-    },
-    devServer: {
-        static: './dist',
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                        },
-                    },
-                ],
+  mode: 'development',
+  entry: './src/main.tsx',
+  devtool: 'inline-source-map',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    static: './dist',
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    plugins: [new TsconfigPathsPlugin()],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
             },
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-            },
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
+          },
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-        }),
-        new MiniCssExtractPlugin(),
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new MiniCssExtractPlugin(),
+  ],
 }
