8f51d62cbe5cb7b740e5548906bdd0d46d5623dd
Taranveer (Taran) Bains <taranveer786@gmail.com>
2023-02-06 10:31:56 -0800

    Update with an example

--- libs/new-lib/src/lib/new-lib.tsx
+++ libs/new-lib/src/lib/new-lib.tsx
@@ -1,15 +1,27 @@
+import React from 'react'
 import styles from './new-lib.module.css'
 
 /* eslint-disable-next-line */
-export interface NewLibProps {}
+export interface NewLibProps {
+  count: number
+  setCount: React.Dispatch<React.SetStateAction<number>>
+}
 
-export function NewLib(props: NewLibProps) {
-    return (
-        <div className={styles['container']}>
-            <h1>Welcome to NewLib!</h1>
-            <h2>I'm inside of the nx libs</h2>
-        </div>
-    )
+export function NewLib({ count, setCount }: NewLibProps) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to NewLib!</h1>
+      <h2>I'm inside of the nx libs</h2>
+      <h3>The count is {count}</h3>
+      <button
+        onClick={() => {
+          setCount((a: number) => a + 1)
+        }}
+      >
+        Increase the count
+      </button>
+    </div>
+  )
 }
 
 export default NewLib
--- src/client/yolo.tsx
+++ src/client/yolo.tsx
@@ -1,12 +1,14 @@
 import { NewLib } from 'new-lib'
+import React from 'react'
 import styles from './yolo.module.css'
 
 function Yolo() {
-    return (
-        <div className={styles?.container}>
-            For the win baby from old app if you know
-            <NewLib />
-        </div>
-    )
+  const [count, setCount] = React.useState(0)
+  return (
+    <div className={styles?.container}>
+      For the win baby from old app if you know
+      <NewLib count={count} setCount={setCount} />
+    </div>
+  )
 }
 export default Yolo
